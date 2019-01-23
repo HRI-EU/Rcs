3b8281793cfe271cb8ffa1c983cfc84fc44f690c
Michael Gienger <michael.gienger@honda-ri.de>
2019-01-23 13:05:40 +0100

    Added hints and continuous integration files

---
+++ pkgInfo.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+#  Custom package settings
+#
+#  Copyright (C)
+#  Honda Research Institute Europe GmbH
+#  Carl-Legien-Str. 30
+#  63073 Offenbach/Main
+#  Germany
+#
+#  UNPUBLISHED PROPRIETARY MATERIAL.
+#  ALL RIGHTS RESERVED.
+#
+#
+name = "Rcs"
+
+category="Libraries"
+
+sqLevel          = "advanced"
+
+sqOptOutRules    = ["GEN10","C04","DOC04"]
+
+sqComments       = {"GEN10":"Software will be hosted in a public Git repository","C04":"Not applicable for C++","DOC04":"Should not be public outside institute"}
+
+
+# EOF
