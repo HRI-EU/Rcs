19e6fa53a13449b349fe703dcd292625bdb5a757
mgienger <michael.gienger@honda-ri.de>
2020-04-23 16:20:54 +0200

    Fixed installation procedure with BST.py

---
+++ postInstallHook.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#  additional steps for the installation procedure
+#
+#  Copyright (C)
+#  Honda Research Institute Europe GmbH
+#  Carl-Legien-Str. 30
+#  63073 Offenbach/Main
+#  Germany
+#
+#  UNPUBLISHED PROPRIETARY MATERIAL.
+#  ALL RIGHTS RESERVED.
+#
+#
+
+
+cd build/${MAKEFILE_PLATFORM}
+make install
+mv ${SIT}/Libraries/Rcs/Rcs/doc/html ${SIT}/Libraries/Rcs/doc/
+rm -fr ${SIT}/Libraries/Rcs/Rcs
+
+
+# EOF
