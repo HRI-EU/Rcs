bd48b99da53e73ec4f3684622b1bdd868b837f21
mgienger <michael.gienger@honda-ri.de>
2020-04-23 17:23:40 +0200

    Updated pkgInfo.py

--- pkgInfo.py
+++ pkgInfo.py
@@ -34,10 +34,9 @@ category="Libraries"
 
 sqLevel = "basic"
 
-sqOptOutRules    = [ 'GEN04', 'PY05', 'C02', 'C06' ]
+sqOptOutRules    = [ 'GEN04', 'C02', 'C06' ]
 
 sqComments       = { 'GEN04': 'Copyright header with BSD license not recognized by quality checker',
-                     'PY05': 'No python code contained. ',
                      'C02': 'Link guards not to be used in Cpp headers ',
                      'C06': 'inline in template declarations should be ok '}
 
