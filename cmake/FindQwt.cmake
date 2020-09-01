5f86fa652931d396c422263b17bd385190de53ca
mgienger <michael.gienger@honda-ri.de>
2020-09-01 16:58:17 +0200

    Enforcing qwt-qt5

--- cmake/FindQwt.cmake
+++ cmake/FindQwt.cmake
@@ -1,20 +1,6 @@
-# Qt Widgets for Technical Applications
-# available at http://www.http://qwt.sourceforge.net/
-#
-# The module defines the following variables:
-#  QWT_FOUND - the system has Qwt
-#  QWT_INCLUDE_DIR - where to find qwt_plot.h
-#  QWT_INCLUDE_DIRS - qwt includes
-#  QWT_LIBRARY - where to find the Qwt library
-#  QWT_LIBRARIES - aditional libraries
-#  QWT_MAJOR_VERSION - major version
-#  QWT_MINOR_VERSION - minor version
-#  QWT_PATCH_VERSION - patch version
-#  QWT_VERSION_STRING - version (ex. 5.2.1)
-#  QWT_ROOT_DIR - root dir (ex. /usr/local)
-
 #=============================================================================
 # Copyright 2010-2013, Julien Schueller
+# Copyright 2018-2020, Rolf Eike Beer
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -42,10 +28,14 @@
 # either expressed or implied, of the FreeBSD Project.
 #=============================================================================
 
+if (Qt5Gui_FOUND)
+  get_target_property(QT_INCLUDE_DIR Qt5::Gui INTERFACE_INCLUDE_DIRECTORIES)
+endif ()
+
 find_path ( QWT_INCLUDE_DIR
   NAMES qwt_plot.h
   HINTS ${QT_INCLUDE_DIR}
-  PATH_SUFFIXES qwt-qt5 #qwt-qt4 qwt-qt3 qwt
+  PATH_SUFFIXES qwt qwt-qt5 qwt6
 )
 
 set ( QWT_INCLUDE_DIRS ${QWT_INCLUDE_DIR} )
@@ -55,63 +45,29 @@ set ( _VERSION_FILE ${QWT_INCLUDE_DIR}/qwt_global.h )
 if ( EXISTS ${_VERSION_FILE} )
   file ( STRINGS ${_VERSION_FILE} _VERSION_LINE REGEX "define[ ]+QWT_VERSION_STR" )
   if ( _VERSION_LINE )
-    string ( REGEX REPLACE ".*define[ ]+QWT_VERSION_STR[ ]+\"(.*)\".*" "\\1" QWT_VERSION_STRING "${_VERSION_LINE}" )
-    string ( REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)" "\\1" QWT_MAJOR_VERSION "${QWT_VERSION_STRING}" )
-    string ( REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)" "\\2" QWT_MINOR_VERSION "${QWT_VERSION_STRING}" )
-    string ( REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)" "\\3" QWT_PATCH_VERSION "${QWT_VERSION_STRING}" )
+    string ( REGEX REPLACE ".*define[ ]+QWT_VERSION_STR[ ]+\"([^\"]*)\".*" "\\1" QWT_VERSION_STRING "${_VERSION_LINE}" )
   endif ()
 endif ()
-
-
-# check version
-set ( _QWT_VERSION_MATCH TRUE )
-if ( Qwt_FIND_VERSION AND QWT_VERSION_STRING )
-  if ( Qwt_FIND_VERSION_EXACT )
-    if ( NOT Qwt_FIND_VERSION VERSION_EQUAL QWT_VERSION_STRING )
-      set ( _QWT_VERSION_MATCH FALSE )
-    endif ()
-  else ()
-    if ( QWT_VERSION_STRING VERSION_LESS Qwt_FIND_VERSION )
-      set ( _QWT_VERSION_MATCH FALSE )
-    endif ()
-  endif ()
-endif ()
-
+unset ( _VERSION_FILE )
 
 find_library ( QWT_LIBRARY
-  NAMES qwt-qt5 #qwt-qt4 qwt-qt3 qwt
+  NAMES qwt-qt5
   HINTS ${QT_LIBRARY_DIR}
 )
 
 set ( QWT_LIBRARIES ${QWT_LIBRARY} )
 
-
-# try to guess root dir from include dir
-if ( QWT_INCLUDE_DIR )
-  string ( REGEX REPLACE "(.*)/include.*" "\\1" QWT_ROOT_DIR ${QWT_INCLUDE_DIR} )
-# try to guess root dir from library dir
-elseif ( QWT_LIBRARY )
-  string ( REGEX REPLACE "(.*)/lib[/|32|64].*" "\\1" QWT_ROOT_DIR ${QWT_LIBRARY} )
-endif ()
-
-
-# handle the QUIETLY and REQUIRED arguments
 include ( FindPackageHandleStandardArgs )
-if ( CMAKE_VERSION LESS 2.8.3 )
-  find_package_handle_standard_args( Qwt DEFAULT_MSG QWT_LIBRARY QWT_INCLUDE_DIR _QWT_VERSION_MATCH )
-else ()
-  find_package_handle_standard_args( Qwt REQUIRED_VARS QWT_LIBRARY QWT_INCLUDE_DIR _QWT_VERSION_MATCH VERSION_VAR QWT_VERSION_STRING )
-endif ()
+find_package_handle_standard_args( Qwt REQUIRED_VARS QWT_LIBRARY QWT_INCLUDE_DIR VERSION_VAR QWT_VERSION_STRING )
 
+if (Qwt_FOUND AND NOT TARGET Qwt::Qwt)
+  add_library(Qwt::Qwt UNKNOWN IMPORTED)
+  set_target_properties(Qwt::Qwt PROPERTIES
+                        INTERFACE_INCLUDE_DIRECTORIES "${QWT_INCLUDE_DIRS}"
+                        IMPORTED_LOCATION "${QWT_LIBRARIES}")
+endif ()
 
 mark_as_advanced (
-  QWT_LIBRARY 
-  QWT_LIBRARIES
+  QWT_LIBRARY
   QWT_INCLUDE_DIR
-  QWT_INCLUDE_DIRS
-  QWT_MAJOR_VERSION
-  QWT_MINOR_VERSION
-  QWT_PATCH_VERSION
-  QWT_VERSION_STRING
-  QWT_ROOT_DIR
 )
